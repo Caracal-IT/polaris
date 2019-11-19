e8fc7cb17a71db1db42c4e72404b2d6768aaaf10
Ettiene Mare <Ettiene.Mare@derivco.co.za>
2019-11-19 11:36:26 +0200

    Add configuration files.

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"polaris/config"
 	"polaris/controllers"
 )
 
 func main() {
+	addr := config.GetConfig().Address
+
 	http.HandleFunc("/", home.Index)
 
-	http.Handle("/favicon.ico", http.NotFoundHandler() )
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
-	log.Fatal(http.ListenAndServe(":8282", nil))
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
