22ed0a9b2acc737adec6f24859f17a60ad7c5926
Ettiene Mare <ettienemare@gmail.com>
2020-02-20 23:20:26 +0200

    fix the validation

--- src/validators/required.validator.ts
+++ src/validators/required.validator.ts
@@ -10,24 +10,9 @@ export interface RequiredValidatorConfig {
 export class RequiredValidator extends Validator {
     validate(context: Context, control: Control, config: RequiredValidatorConfig): boolean {
         const value = context.model.getValue(control.id);
-        
-        control.error = false;
-        delete control.errorMessage;
-
-        if (value === null || value === undefined || value.length === 0) {            
-            control.error = true;
-            control.errorMessage = config.message;
-        }
-
-        if(control.el) {
-            control.el.setAttribute("error", control.error ? "true" : "false");
-            control.el.setAttribute("errorMessage", control.errorMessage);
-        }
-
-        if(control.el.nextSibling["attributes"]["wf-error"])
-            control.el.nextSibling.textContent = control.errorMessage;
-
+        const isEmpty = value === null || value === undefined || value.length === 0;
+        super.setError(control, isEmpty, config.message);
               
-        return !control.error;
+        return !isEmpty;
     }
 }
--- src/validators/validator.ts
+++ src/validators/validator.ts
@@ -4,4 +4,17 @@ import { Control } from "../model/control.model";
 export abstract class Validator {
     constructor(public name: string) { }
     abstract validate(context: Context, control: Control, config: any): boolean;
+
+    setError(control: Control, error: boolean, message?: string) {
+        control.error = error;
+        control.errorMessage = error ? message : null;
+
+        if(control.el) {
+            control.el.setAttribute("error", control.error ? "true" : "false");
+            control.el.setAttribute("errorMessage", control.errorMessage);
+        }
+
+        if(control.el.nextSibling["attributes"]["wf-error"])
+            control.el.nextSibling.textContent = control.errorMessage;
+    }
 }
