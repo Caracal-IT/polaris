7fb1abfc90a0ae631c617ea28f25863a1f91272f
Ettiene Mare <Ettiene.Mare@derivco.co.za>
2020-02-20 08:28:40 +0200

    Remove old files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # polaris
 Create an admin shell, where you can configure modules.
+
+Run npm install
+Run npm start
--- www/build/polaris.esm.js
+++
@@ -1,1 +0,0 @@
-import{p as a,b as o}from"./p-51f32ca9.js";a().then(a=>o([["p-1keqtagf",[[0,"polaris-analytics",null,[[4,"click","analyticsHandler"],[4,"wfMessage","wfMessage"]]],[0,"polaris-workflow",{tag:[1],ctx:[16],value:[8],process:[1],load:[64]}]]],["p-yluxyt8k",[[1,"moon-button",{ctx:[16],caption:[1],next:[1]}]]],["p-emnglcwo",[[1,"moon-panel",{caption:[1]}]]]],a));
