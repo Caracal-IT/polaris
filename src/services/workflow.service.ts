b9026e101fce02cdcc87b64281885276025557ab
Ettiene Mare <ettienemare@gmail.com>
2020-02-22 22:20:00 +0200

    fix loading

--- src/services/workflow.service.ts
+++ src/services/workflow.service.ts
@@ -19,10 +19,8 @@ export class WorkflowService {
     
     async setProcess(process: any, next = "start") {
         try {
-            if(typeof process === "string") { 
-                this.ctx.page.sendMessage({type: "START_LOADING"});
+            if(typeof process === "string") 
                 process = await this.ctx.http.fetch({url: `[WF]/${process}`, method: 'get'});
-            }
 
             this.process = process; 
             this.activity = null;
