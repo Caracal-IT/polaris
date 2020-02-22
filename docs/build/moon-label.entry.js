4793c6c80b1088d0926f7d72e9596fa05c5f4eb4
Ettiene Mare <ettienemare@gmail.com>
2020-02-22 18:55:45 +0200

    fix formatting

---
+++ docs/build/moon-label.entry.js
@@ -0,0 +1,13 @@
+import { r as registerInstance, h } from './core-0e88e524.js';
+
+const MoonBLabel = class {
+    constructor(hostRef) {
+        registerInstance(this, hostRef);
+    }
+    render() {
+        return h("span", null, this.caption);
+    }
+    static get style() { return ""; }
+};
+
+export { MoonBLabel as moon_label };
