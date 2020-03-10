93f7942a3ae293fd980bca00a5c3923200d94837
Ettiene Mare <Ettiene.Mare@derivco.co.za>
2020-03-10 12:24:06 +0200

    fix assign activity

--- src/activities/switch.activity.ts
+++ src/activities/switch.activity.ts
@@ -3,18 +3,18 @@ import { Context } from "../model/context.model";
 
 export class AssignActivity implements Activity {
     name = "assign";
-    type = "assign-activity";
+    type = "switch-activity";
 
     ctx: Context;
     key: string;
-    value: string;
-    next: string;
+   
     
     async execute(): Promise<boolean> {
-        const value = this.ctx.model.getInterpolatedValue(this.value);            
+       // let value = this.value||"";
+
                 
-        this.ctx.model.setValue(this.key, value);         
-        this.ctx.wf.goto(this.next);
+        //this.ctx.model.setValue(this.key, value);         
+        //this.ctx.wf.goto(this.next);
             
         return true;
     }
