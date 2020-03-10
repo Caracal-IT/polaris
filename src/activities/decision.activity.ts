9d2b73e2cd6897563bd941549e408fd9fd5a420d
Ettiene Mare <ettienemare@gmail.com>
2020-03-10 18:53:18 +0200

    refactor the decision activity.

--- src/activities/decision.activity.ts
+++ src/activities/decision.activity.ts
@@ -1,55 +1,23 @@
 import { CodeActivity } from './code.activity';
 import { Context } from "../model/context.model";
 
-interface Condition {
-    operator: 'and'|'or'|undefined|null;
-    left: string;
-    equality: string;
-    right: string;
-}
-
 export class DecisionActivity extends CodeActivity {
     name = "assign";
     type = "decision-activity";
 
     ctx: Context;
+    expression: string;
     trueAction: string;
     falseAction: string;    
-    conditions: Array<Condition>;
     
     async execute(): Promise<boolean> {
-        if(!this.conditions)
-            return false;
-
-        let isValid: boolean;
-
-        for(let condition of this.conditions) {
-            if(isValid === undefined) 
-                isValid = this.validate(condition);
-            else if(condition.operator === 'or')
-                isValid = isValid || this.validate(condition);
-            else
-                isValid = isValid && this.validate(condition);            
-        }
-
-        if(isValid)
+        const exp  = `return ${this.ctx.model.getInterpolatedValue(this.expression)};`;
+           
+        if(this.eval(exp, this.ctx)) 
             this.ctx.wf.goto(this.trueAction);
         else
             this.ctx.wf.goto(this.falseAction);
-            
-        return true;
-    }
 
-    private validate(condition: Condition) {
-        try {
-            let left = this.ctx.model.getInterpolatedValue(condition.left);
-            const expression = `return ${left} ${condition.equality} ${condition.right};`
-            
-            return super.eval(expression, this.ctx);
-        }
-        catch(ex) {
-            console.warn('Decision Activity', ex);
-            return false;
-        }
+        return true;
     }
 }
