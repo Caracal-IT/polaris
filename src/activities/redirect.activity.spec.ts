d43b8ef14020fd1495791ebcc16d2d9e71521193
Ettiene Mare <ettienemare@gmail.com>
2020-03-12 17:40:37 +0200

    finish the redirect activity test.

--- src/activities/redirect.activity.spec.ts
+++ src/activities/redirect.activity.spec.ts
@@ -1,16 +1,25 @@
 import { RedirectActivity } from "./redirect.activity";
 import { ModelService } from "../services/model.service";
 import { WorkflowService } from "../services/workflow.service";
+import { Context } from "../model/context.model";
 
 describe('activities/redirect-activity', () => {
-    const context = {
-        page: null,
-        http: null,
-        model: new ModelService(null),
-        wf: null,
-        config: null,
-        validator: null
-    };
+    let win: any = window;
+    let context: Context;
+
+    beforeEach(() => {
+        context = {
+            page: null,
+            http: null,
+            model: new ModelService(null),
+            wf: new WorkflowService(null),
+            config: null,
+            validator: null
+        }
+
+        context.model.save = jest.fn(); 
+        win.location = {};         
+    });
     
     it('builds', () => {
         expect(new RedirectActivity()).toBeTruthy();
@@ -20,19 +29,31 @@ describe('activities/redirect-activity', () => {
         expect(new RedirectActivity().name).toBe('redirect');
     });
 
-    it('should have a switch activity type', () => {
+    it('should have a switch redirect type', () => {
         expect(new RedirectActivity().type).toBe('redirect-activity');
     });
 
     it('should save state before redirect', async () => {
-        const act = new RedirectActivity();  
-        const wf = new WorkflowService(null);               
-        act.ctx = { ...context, wf:wf };
-        act.ctx.model.save = jest.fn(); 
+        const act = new RedirectActivity();                
+        act.ctx = context;
                 
         expect.assertions(2);        
 
         await expect(act.execute()).resolves.toEqual(true);
         expect(act.ctx.model.save).toHaveBeenCalled();                
+    });
+
+    it('should redirect page', async () => {
+        const act = new RedirectActivity();                 
+        act.ctx = context;
+        act.name = 'myAct';
+        act.location = 'myLocation';
+        act.next = 'myNextAct';
+
+        act.ctx.model.sessionId = 'mySession';
+        act.ctx.wf.process = {name: 'myProcess', activities: []};
+
+        await expect(act.execute()).resolves.toEqual(true);
+        expect(win.location.href).toBe('myLocation?returnUrl=myProcess-myNextAct-mySession');              
     });
 });
