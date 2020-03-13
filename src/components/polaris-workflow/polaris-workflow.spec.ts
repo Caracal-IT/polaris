9823ea652acfd3ecf734d17baa5d574f6d331230
Ettiene Mare <ettienemare@gmail.com>
2020-03-13 21:02:39 +0200

    remove wrong test

--- src/components/polaris-workflow/polaris-workflow.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PolarisWorkflow } from "./polaris-workflow";
-
-describe('components/polaris-workflow', () => {
-    it('builds', () => {
-      expect(new PolarisWorkflow()).toBeTruthy();
-    });
-});
