bdd62d29147c8a1f7d9df717b9970bcb4448c533
ettienemare <ettienemare@gmail.com>
2022-09-30 19:21:25 +0200

    Fix code issues

--- src/activities/page.activity.ts
+++ src/activities/page.activity.ts
@@ -3,15 +3,17 @@ import { Context } from "../model/context.model";
 import { Control } from "../model/control.model";
 
 export class PageActivity implements Activity {
-    name = "start";
-    "type" = "page-activity";
+    name: string = "start";
+    "type": string = "page-activity";
     
     ctx: Context;
     controls: Control[];
 
     async execute(): Promise<boolean> {
         if (this.ctx.page.controls !== null)
-            this.ctx.page.controls = this.controls || [];
+            this.ctx.page.controls = this.controls;
+        else
+            this.ctx.page.controls = [];
             
         return true;
     }
